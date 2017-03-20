decfd89e7c00f27599df4bdfd513ee76d1c84f91
Sny <sny.aggarwal@gmail.com>
2017-03-20 12:12:56 +0530

    + @rajatkukreja | #3 | using treadmill egg path for default ansible playbook options

--- treadmill/cli/aws.py
+++ treadmill/cli/aws.py
@@ -2,7 +2,8 @@
 from __future__ import absolute_import
 
 import logging
-
+import treadmill
+import os
 import click
 from ansible.cli.playbook import PlaybookCLI
 
@@ -17,14 +18,23 @@ def init():
         """Manage treadmill on AWS"""
         pass
 
+    def _get_from_treadmill_egg(obj):
+        treadmill_egg_path = treadmill.__path__
+        return os.path.join(treadmill_egg_path[0], '../ansible/', obj)
+
     @aws.command(name='cell')
-    @click.option('--create', required=False, is_flag=True,
+    @click.option('--create',
+                  required=False,
+                  is_flag=True,
                   help='Create a new treadmill cell on AWS',)
-    @click.option('--playbook', default='cell.yml',
+    @click.option('--playbook',
+                  default=_get_from_treadmill_egg('cell.yml'),
                   help='Playbok file',)
-    @click.option('--inventory', default='controller.inventory',
+    @click.option('--inventory',
+                  default=_get_from_treadmill_egg('controller.inventory'),
                   help='Inventory file',)
-    @click.option('--key-file', default='key.pem',
+    @click.option('--key-file',
+                  default='key.pem',
                   help='AWS ssh pem file',)
     def cell(create, playbook, inventory, key_file):
         """Manage treadmill cell on AWS"""
@@ -41,13 +51,18 @@ def init():
             playbook_cli.run()
 
     @aws.command(name='node')
-    @click.option('--create', required=False, is_flag=True,
+    @click.option('--create',
+                  required=False,
+                  is_flag=True,
                   help='Create a new treadmill node',)
-    @click.option('--playbook', default='node.yml',
+    @click.option('--playbook',
+                  default=_get_from_treadmill_egg('node.yml'),
                   help='Playbok file',)
-    @click.option('--inventory', default='controller.inventory',
+    @click.option('--inventory',
+                  default=_get_from_treadmill_egg('controller.inventory'),
                   help='Inventory file',)
-    @click.option('--key-file', default='key.pem',
+    @click.option('--key-file',
+                  default='key.pem',
                   help='AWS ssh pem file',)
     def node(create, playbook, inventory, key_file):
         """Manage treadmill node"""
