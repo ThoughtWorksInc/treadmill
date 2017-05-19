b3ebc2b8cad5ce7a7091a13e9fc13f485353040a
Sny <sny.aggarwal@gmail.com>
2017-05-19 08:40:53 +0530

    python3 | dict() compare doesnt work

--- treadmill/api/app.py
+++ treadmill/api/app.py
@@ -45,7 +45,7 @@ class API(object):
                 app for app in apps
                 if fnmatch.fnmatch(app['_id'], match)
             ]
-            return sorted(filtered)
+            return sorted(filtered, key=lambda x: x['_id'])
 
         @schema.schema({'$ref': 'app.json#/resource_id'})
         def get(rsrc_id):
