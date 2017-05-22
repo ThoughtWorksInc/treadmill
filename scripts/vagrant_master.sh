859c12db8c382962c7b3c74aa510d938031051c6
Gaurav Butola <gbutola@thoughtworks.com>
2017-05-22 17:22:00 +0530

    + @pooja-dhupar | #48 | Added ldap configurations and file permissions

--- scripts/vagrant_master.sh
+++ scripts/vagrant_master.sh
@@ -8,6 +8,29 @@ yum install java wget openldap-servers -y
 echo 'Setting up Zookeeper'
 /home/treadmld/treadmill/scripts/zk_setup.sh
 
+cp "etc/ldap/treadmill.ldif" "/etc/openldap/slapd.d/cn=config/cn=schema/cn={1}treadmill.ldif"
+chown ldap:ldap "/etc/openldap/slapd.d/cn=config/cn=schema/cn={1}treadmill.ldif"
+chmod 0600 "/etc/openldap/slapd.d/cn=config/cn=schema/cn={1}treadmill.ldif"
+
+cp "etc/ldap/config.ldif" "/etc/openldap/slapd.d/cn=config/olcDatabase={0}config.ldif"
+chown ldap:ldap "/etc/openldap/slapd.d/cn=config/olcDatabase={0}config.ldif"
+chmod 0600 "/etc/openldap/slapd.d/cn=config/olcDatabase={0}config.ldif"
+
+cp "etc/ldap/hdb.ldif" "/etc/openldap/slapd.d/cn=config/olcDatabase={2}hdb.ldif"
+chown ldap:ldap "/etc/openldap/slapd.d/cn=config/olcDatabase={2}hdb.ldif"
+chmod 0600 "/etc/openldap/slapd.d/cn=config/olcDatabase={2}hdb.ldif"
+
+cp "etc/ldap/slapd" "/etc/sysconfig/slapd"
+chown root:root "/etc/sysconfig/slapd"
+chmod 0644 "/etc/sysconfig/slapd"
+
+chown -R ldap /var/lib/ldap
+chgrp -R ldap /var/lib/ldap
+
 sudo service slapd start
+
+treadmill admin ldap init master
+treadmill admin ldap schema --load treadmill/etc/ldap/schema.yml
+treadmill admin ldap cell configure -u admin local
 
 echo 'master provisioned!'
