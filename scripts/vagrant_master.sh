9811a2f9dcbeac3553d0071089ebda77e941a235
Gaurav Butola <gbutola@thoughtworks.com>
2017-05-23 13:17:00 +0530

    + @pooja-dhupar | #48 | Vagrant master provision script refactoring

--- scripts/vagrant_master.sh
+++ scripts/vagrant_master.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
+TREADMILL_CODE="/home/treadmld/treadmill/"
 
 echo 'password' > /home/treadmld/.treadmill_ldap
 
@@ -8,29 +9,27 @@ yum install java wget openldap-servers -y
 echo 'Setting up Zookeeper'
 /home/treadmld/treadmill/scripts/zk_setup.sh
 
-cp "etc/ldap/treadmill.ldif" "/etc/openldap/slapd.d/cn=config/cn=schema/cn={1}treadmill.ldif"
+cp $TREADMILL_CODE"etc/ldap/treadmill.ldif" "/etc/openldap/slapd.d/cn=config/cn=schema/cn={1}treadmill.ldif"
 chown ldap:ldap "/etc/openldap/slapd.d/cn=config/cn=schema/cn={1}treadmill.ldif"
 chmod 0600 "/etc/openldap/slapd.d/cn=config/cn=schema/cn={1}treadmill.ldif"
 
-cp "etc/ldap/config.ldif" "/etc/openldap/slapd.d/cn=config/olcDatabase={0}config.ldif"
+cp $TREADMILL_CODE"etc/ldap/config.ldif" "/etc/openldap/slapd.d/cn=config/olcDatabase={0}config.ldif"
 chown ldap:ldap "/etc/openldap/slapd.d/cn=config/olcDatabase={0}config.ldif"
 chmod 0600 "/etc/openldap/slapd.d/cn=config/olcDatabase={0}config.ldif"
 
-cp "etc/ldap/hdb.ldif" "/etc/openldap/slapd.d/cn=config/olcDatabase={2}hdb.ldif"
+cp $TREADMILL_CODE"etc/ldap/hdb.ldif" "/etc/openldap/slapd.d/cn=config/olcDatabase={2}hdb.ldif"
 chown ldap:ldap "/etc/openldap/slapd.d/cn=config/olcDatabase={2}hdb.ldif"
 chmod 0600 "/etc/openldap/slapd.d/cn=config/olcDatabase={2}hdb.ldif"
 
-cp "etc/ldap/slapd" "/etc/sysconfig/slapd"
+cp $TREADMILL_CODE"etc/ldap/slapd" "/etc/sysconfig/slapd"
 chown root:root "/etc/sysconfig/slapd"
 chmod 0644 "/etc/sysconfig/slapd"
 
-chown -R ldap /var/lib/ldap
-chgrp -R ldap /var/lib/ldap
+chown -R ldap:ldap /var/lib/ldap
+
+# Set nis_enabled selinux boolean
+setsebool -P nis_enabled on
 
 sudo service slapd start
-
-treadmill admin ldap init master
-treadmill admin ldap schema --load treadmill/etc/ldap/schema.yml
-treadmill admin ldap cell configure -u admin local
 
 echo 'master provisioned!'
