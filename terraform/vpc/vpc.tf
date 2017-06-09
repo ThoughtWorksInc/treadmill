d0d020e08fe7cf699df9a6e20caa086162ba6dd5
Sny <sny.aggarwal@gmail.com>
2017-06-09 17:39:46 +0530

    keeping code format consistent

--- terraform/vpc/vpc.tf
+++ terraform/vpc/vpc.tf
@@ -92,9 +92,11 @@ resource "aws_security_group" "secgroup" {
 output "vpc_id" {
   value = "${aws_vpc.vpc.id}"
 }
+
 output "subnet_id" {
   value = "${aws_subnet.subnet.id}"
 }
+
 output "secgroup_id" {
   value = "${aws_security_group.secgroup.id}"
 }
