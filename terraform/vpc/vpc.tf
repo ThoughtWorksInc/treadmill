e26e77a99d46c9f6af36d97e952f15af196dae43
Sny <sny.aggarwal@gmail.com>
2017-06-09 17:23:47 +0530

    vpc compacted

--- terraform/vpc/vpc.tf
+++ terraform/vpc/vpc.tf
@@ -1,3 +1,37 @@
+variable "region" {}
+
+variable "vpc_name" {
+  default = "Treadmill"
+}
+
+variable "vpc_cidr" {
+  default = "172.23.0.0/16"
+}
+
+variable "subnet_name" {
+  default = "Treadmill"
+}
+
+variable "subnet_cidr" {
+  default = "172.23.1.0/24"
+}
+
+variable "securtiygrp_name" {
+  default = "sg_common"
+}
+
+variable "az" {
+  type = "map"
+  default = {
+    "us-east-1" = "us-east-1a"
+    "us-east-2" = "us-east-2a"
+    "ap-southeast-1" = "ap-southeast-1a"
+    "ap-southeast-2" = "ap-southeast-2a"
+    "us-west-1" = "us-west-1a"
+    "us-west-2" = "us-west-2a"
+  }
+}
+
 resource "aws_vpc" "vpc" {
   cidr_block = "${var.vpc_cidr}"
   tags {
@@ -53,4 +87,14 @@ resource "aws_security_group" "sg" {
     protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+output "vpc_id" {
+  value = "${aws_vpc.vpc.id}"
+}
+output "subnet_id" {
+  value = "${aws_subnet.subnet.id}"
+}
+output "sg_id" {
+  value = "${aws_security_group.sg.id}"
 }
