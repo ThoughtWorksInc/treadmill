bb7c50a2a3af664b5239fb5b61957a39a8330627
Sny <sny.aggarwal@gmail.com>
2017-06-12 10:47:29 +0530

    master and node provisioning

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,3 +15,21 @@ module "freeipa" {
   secgroup_id = "${module.vpc.secgroup_id}"
   region = "${var.region}"
 }
+
+module "treadmill-master" {
+  source = "instance"
+  role = "TreadmillMaster"
+  count = "3"
+  subnet_id = "${module.vpc.subnet_id}"
+  secgroup_id = "${module.vpc.secgroup_id}"
+  region = "${var.region}"
+}
+
+module "treadmill-node" {
+  source = "instance"
+  role = "TreadmillNode"
+  count = "1"
+  subnet_id = "${module.vpc.subnet_id}"
+  secgroup_id = "${module.vpc.secgroup_id}"
+  region = "${var.region}"
+}
