9835711a99230c5ae734ab920364fcf3a11032c0
Gaurav Butola <gbutola@thoughtworks.com>
2017-06-27 12:44:03 +0530

    + @rajatkukreja | Run treadmill master as part of master configuration

--- treadmill/infra/setup/scripts/configure-master.sh
+++ treadmill/infra/setup/scripts/configure-master.sh
@@ -1,6 +1,8 @@
 LDAP_DC=ou=treadmill,$(echo "{{ DOMAIN }}" | sed -E 's/([a-z]*)\.([a-z]*)/dc=\1,dc=\2/g')
 LDAP_URL=ldap://{{ FREEIPA_HOSTNAME }}.{{ DOMAIN }}:1389
 ZK_URL=zookeeper://foo@TreadmillZookeeper1.{{ DOMAIN }}:2181,TreadmillZookeeper2.{{ DOMAIN }}:2181,TreadmillZookeeper3.{{ DOMAIN }}:2181
+AMI_LAUNCH_INDEX=$(curl -s http://169.254.169.254/latest/meta-data/ami-launch-index)
+MASTER_ID=$(expr $AMI_LAUNCH_INDEX + 1) # AMI_LAUNCH_INDEX is 0 indexed, master cannot be set to 0.
 
 curl -L "https://github.com/ThoughtWorksInc/treadmill/releases/download/{{ TREADMILL_RELEASE }}/treadmill" -o /bin/treadmill
 
@@ -16,3 +18,6 @@ export TREADMILL_LDAP=$LDAP_URL
 export TREADMILL_LDAP_SEARCH_BASE=${LDAP_DC}
 EOF
 ) >> /root/.bashrc
+
+# Start master service
+. /root/.bashrc && nohup treadmill admin install --profile aws master --master-id ${MASTER_ID}  --run > master_services.out 2>&1 &
