f9e0d5ec4aac85321f14ca3e2f6283357d1c381b
Sny <sny.aggarwal@gmail.com>
2017-07-02 11:47:03 +0530

    freeipa srv/txt/A records are done

--- tests/infra/test_setup/test_freeipa.py
+++ tests/infra/test_setup/test_freeipa.py
@@ -11,24 +11,201 @@ from treadmill.infra.setup.freeipa import FreeIPA
 class FreeIPATest(unittest.TestCase):
     """Tests EC2 freeipa setup."""
 
+    @mock.patch('treadmill.infra.setup.freeipa.connection.Connection')
+    @mock.patch('treadmill.infra.setup.freeipa.vpc.VPC')
     @mock.patch('treadmill.infra.setup.freeipa.instances.Instances')
-    def test_setup_freeipa(self, InstancesMock):
-        InstancesMock.create = mock.Mock(return_value=['foo'])
-
+    def test_setup_freeipa(self, InstancesMock, VPCMock, ConnectionMock):
+        instance_mock = mock.Mock(private_ip='1.1.1.1')
+        instances_mock = mock.Mock(instances=[instance_mock])
+        InstancesMock.create = mock.Mock(return_value=instances_mock)
+        conn_mock = ConnectionMock('route53')
+        _vpc_id_mock = 'vpc-id'
+        _vpc_mock = VPCMock(_vpc_id_mock)
+        _vpc_mock.hosted_zone_id = 'hosted-zone-id'
         freeipa = FreeIPA()
         freeipa.setup(
             name='freeipa',
             image_id='foo-123',
             count=1,
-            subnet_id=123
+            subnet_id=123,
+            domain='foo.bar',
+            vpc_id=_vpc_id_mock
         )
 
-        self.assertEqual(freeipa.instances, ['foo'])
+        self.assertEqual(freeipa.instances, instances_mock)
         InstancesMock.create.assert_called_once_with(
             image_id='foo-123',
             name='freeipa',
             count=1,
-            subnet_id=123
+            subnet_id=123,
+            instance_type='t2.medium'
+        )
+        _vpc_mock.get_hosted_zone_ids.assert_called_once()
+        expected_calls = [
+            mock.mock.call(
+                HostedZoneId='hosted-zone-id',
+                ChangeBatch={
+                    'Changes': [{
+                        'Action': 'UPSERT',
+                        'ResourceRecordSet': {
+                            'Name': '_kerberos-master._tcp.foo.bar.',
+                            'Type': 'SRV',
+                            'TTL': 86400,
+                            'ResourceRecords': [{
+                                'Value': '0 100 88 freeipa.foo.bar.'
+                            }]
+                        }
+                    }]
+                }
+            ),
+            mock.mock.call(
+                HostedZoneId='hosted-zone-id',
+                ChangeBatch={
+                    'Changes': [{
+                        'Action': 'UPSERT',
+                        'ResourceRecordSet': {
+                            'Name': '_kerberos-master._udp.foo.bar.',
+                            'Type': 'SRV',
+                            'TTL': 86400,
+                            'ResourceRecords': [{
+                                'Value': '0 100 88 freeipa.foo.bar.'
+                            }]
+                        }
+                    }]
+                }
+            ),
+            mock.mock.call(
+                HostedZoneId='hosted-zone-id',
+                ChangeBatch={
+                    'Changes': [{
+                        'Action': 'UPSERT',
+                        'ResourceRecordSet': {
+                            'Name': '_kerberos._tcp.foo.bar.',
+                            'Type': 'SRV',
+                            'TTL': 86400,
+                            'ResourceRecords': [{
+                                'Value': '0 100 88 freeipa.foo.bar.'
+                            }]
+                        }
+                    }]
+                }
+            ),
+            mock.mock.call(
+                HostedZoneId='hosted-zone-id',
+                ChangeBatch={
+                    'Changes': [{
+                        'Action': 'UPSERT',
+                        'ResourceRecordSet': {
+                            'Name': '_kerberos._udp.foo.bar.',
+                            'Type': 'SRV',
+                            'TTL': 86400,
+                            'ResourceRecords': [{
+                                'Value': '0 100 88 freeipa.foo.bar.'
+                            }]
+                        }
+                    }]
+                }
+            ),
+            mock.mock.call(
+                HostedZoneId='hosted-zone-id',
+                ChangeBatch={
+                    'Changes': [{
+                        'Action': 'UPSERT',
+                        'ResourceRecordSet': {
+                            'Name': '_kpasswd._tcp.foo.bar.',
+                            'Type': 'SRV',
+                            'TTL': 86400,
+                            'ResourceRecords': [{
+                                'Value': '0 100 464 freeipa.foo.bar.'
+                            }]
+                        }
+                    }]
+                }
+            ),
+            mock.mock.call(
+                HostedZoneId='hosted-zone-id',
+                ChangeBatch={
+                    'Changes': [{
+                        'Action': 'UPSERT',
+                        'ResourceRecordSet': {
+                            'Name': '_kpasswd._udp.foo.bar.',
+                            'Type': 'SRV',
+                            'TTL': 86400,
+                            'ResourceRecords': [{
+                                'Value': '0 100 464 freeipa.foo.bar.'
+                            }]
+                        }
+                    }]
+                }
+            ),
+            mock.mock.call(
+                HostedZoneId='hosted-zone-id',
+                ChangeBatch={
+                    'Changes': [{
+                        'Action': 'UPSERT',
+                        'ResourceRecordSet': {
+                            'Name': '_ldap._tcp.foo.bar.',
+                            'Type': 'SRV',
+                            'TTL': 86400,
+                            'ResourceRecords': [{
+                                'Value': '0 100 389 freeipa.foo.bar.'
+                            }]
+                        }
+                    }]
+                }
+            ),
+            mock.mock.call(
+                HostedZoneId='hosted-zone-id',
+                ChangeBatch={
+                    'Changes': [{
+                        'Action': 'UPSERT',
+                        'ResourceRecordSet': {
+                            'Name': '_ntp._udp.foo.bar.',
+                            'Type': 'SRV',
+                            'TTL': 86400,
+                            'ResourceRecords': [{
+                                'Value': '0 100 123 freeipa.foo.bar.'
+                            }]
+                        }
+                    }]
+                }
+            ),
+            mock.mock.call(
+                HostedZoneId='hosted-zone-id',
+                ChangeBatch={
+                    'Changes': [{
+                        'Action': 'UPSERT',
+                        'ResourceRecordSet': {
+                            'Name': 'ipa-ca.foo.bar.',
+                            'Type': 'A',
+                            'TTL': 86400,
+                            'ResourceRecords': [{
+                                'Value': '1.1.1.1'
+                            }]
+                        }
+                    }]
+                }
+            ),
+            mock.mock.call(
+                HostedZoneId='hosted-zone-id',
+                ChangeBatch={
+                    'Changes': [{
+                        'Action': 'UPSERT',
+                        'ResourceRecordSet': {
+                            'Name': '_kerberos.foo.bar.',
+                            'Type': 'TXT',
+                            'TTL': 86400,
+                            'ResourceRecords': [{
+                                'Value': '"FOO.BAR"'
+                            }]
+                        }
+                    }]
+                }
+            )
+        ]
+        self.assertCountEqual(
+            conn_mock.change_resource_record_sets.mock_calls,
+            expected_calls
         )
 
     @mock.patch('treadmill.infra.setup.freeipa.instances.Instances')
--- treadmill/infra/setup/freeipa.py
+++ treadmill/infra/setup/freeipa.py
@@ -1,18 +1,95 @@
 from treadmill.infra import instances
+from treadmill.infra import connection
+from treadmill.infra import vpc
+from treadmill.infra import constants
 
 
 class FreeIPA:
     def __init__(self):
-        self.instance = None
+        pass
 
-    def setup(self, name, image_id, count, subnet_id):
+    def setup(self, name, image_id, count, subnet_id, vpc_id, domain):
+        self.conn = connection.Connection('route53')
+        self.name = name
+        self.domain = domain
         self.instances = instances.Instances.create(
-            name=name,
+            name=self.name,
             image_id=image_id,
             count=count,
             subnet_id=subnet_id,
+            instance_type=constants.INSTANCE_TYPES['EC2']['medium']
         )
+
+        self._update_route53('UPSERT', vpc_id, domain)
 
     def destroy(self):
         """Terminate freeipa instance"""
         self.instances.terminate()
+
+    def _update_route53(self, action, vpc_id, domain):
+        _vpc = vpc.VPC(vpc_id)
+        _vpc.get_hosted_zone_ids()
+
+        srv_records = {
+            '_kerberos-master._tcp': '0 100 88',
+            '_kerberos-master._udp': '0 100 88',
+            '_kerberos._tcp': '0 100 88',
+            '_kerberos._udp': '0 100 88',
+            '_kpasswd._tcp': '0 100 464',
+            '_kpasswd._udp': '0 100 464',
+            '_ldap._tcp': '0 100 389',
+            '_ntp._udp': '0 100 123'
+        }
+
+        for _rec, _value in srv_records.items():
+            self._change_srv_record(
+                action=action,
+                hosted_zone_id=_vpc.hosted_zone_id,
+                name=self._rec_name(_rec),
+                value=self._srv_rec_value(_value),
+                record_type='SRV'
+            )
+
+        self._change_srv_record(
+            action=action,
+            hosted_zone_id=_vpc.hosted_zone_id,
+            name=self._rec_name('ipa-ca'),
+            value=self.instances.instances[0].private_ip,
+            record_type='A'
+        )
+        self._change_srv_record(
+            action=action,
+            hosted_zone_id=_vpc.hosted_zone_id,
+            name=self._rec_name('_kerberos'),
+            value='"{0}"'.format(domain.upper()),
+            record_type='TXT'
+        )
+
+    def _rec_name(self, name):
+        return name + '.' + self.domain + '.'
+
+    def _srv_rec_value(self, value):
+        return value + ' ' + self.name + '.' + self.domain + '.'
+
+    def _change_srv_record(self,
+                           action,
+                           hosted_zone_id,
+                           name,
+                           value,
+                           record_type):
+        self.conn.change_resource_record_sets(
+            HostedZoneId=hosted_zone_id.split('/')[-1],
+            ChangeBatch={
+                'Changes': [{
+                    'Action': action,
+                    'ResourceRecordSet': {
+                        'Name': name,
+                        'Type': record_type,
+                        'TTL': constants.IPA_ROUTE_53_RECORD_SET_TTL,
+                        'ResourceRecords': [{
+                            'Value': value
+                        }]
+                    }
+                }]
+            }
+        )
