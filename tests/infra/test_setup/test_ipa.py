c03ddbf5b57817b28283830f6a76919b279aeace
Sny <sny.aggarwal@gmail.com>
2017-07-03 10:06:07 +0530

    fixed ipa destroy | making it standalone | destroys srv records

--- tests/infra/test_setup/test_ipa.py
+++ tests/infra/test_setup/test_ipa.py
@@ -20,7 +20,9 @@ class IPATest(unittest.TestCase):
         InstancesMock.create = mock.Mock(return_value=instances_mock)
         conn_mock = ConnectionMock('route53')
         _vpc_id_mock = 'vpc-id'
-        _vpc_mock = VPCMock(_vpc_id_mock)
+        _vpc_mock = VPCMock(id=_vpc_id_mock,
+                            region_name='region',
+                            domain='foo.bar')
         _vpc_mock.hosted_zone_id = 'hosted-zone-id'
         ipa = IPA()
         ipa.setup(
@@ -29,7 +31,8 @@ class IPATest(unittest.TestCase):
             count=1,
             subnet_id=123,
             domain='foo.bar',
-            vpc_id=_vpc_id_mock
+            vpc_id=_vpc_id_mock,
+            region_name='region'
         )
 
         self.assertEqual(ipa.instances, instances_mock)
@@ -208,10 +211,202 @@ class IPATest(unittest.TestCase):
             expected_calls
         )
 
+    @mock.patch('treadmill.infra.setup.ipa.connection.Connection')
+    @mock.patch('treadmill.infra.setup.ipa.vpc.VPC')
     @mock.patch('treadmill.infra.setup.ipa.instances.Instances')
-    def test_ipa_destroy(self, InstancesMock):
+    def test_ipa_destroy(self, InstancesMock, VPCMock, ConnectionMock):
+        instance_mock = mock.Mock(private_ip='1.1.1.1')
+        instances_mock = mock.Mock(instances=[instance_mock])
+        InstancesMock.get = mock.Mock(return_value=instances_mock)
+        conn_mock = ConnectionMock('route53')
+        vpc_mock = VPCMock(
+            id='vpc-id',
+            domain='foo.bar',
+            region_name='region'
+        )
+        vpc_mock.get_hosted_zone_ids = mock.Mock()
+        vpc_mock.hosted_zone_id = 'hosted-zone-id'
+        vpc_mock.reverse_hosted_zone_id = 'reverse-hosted-zone-id'
         ipa = IPA()
         ipa.instances = InstancesMock()
-        ipa.destroy()
+        ipa.destroy(
+            instance_id='instance-id',
+            vpc_id='vpc-id',
+            domain='foo.bar',
+            region_name='region',
+            name='ipa'
+        )
 
-        ipa.instances.terminate.assert_called_once()
+        InstancesMock.get.assert_called_once_with(ids=['instance-id'])
+        vpc_mock.get_hosted_zone_ids.assert_called_once()
+        ipa.instances.terminate.assert_called_once_with(
+            hosted_zone_id='hosted-zone-id',
+            reverse_hosted_zone_id='reverse-hosted-zone-id',
+            domain='foo.bar'
+        )
+        expected_calls = [
+            mock.mock.call(
+                HostedZoneId='hosted-zone-id',
+                ChangeBatch={
+                    'Changes': [{
+                        'Action': 'DELETE',
+                        'ResourceRecordSet': {
+                            'Name': '_kerberos-master._tcp.foo.bar.',
+                            'Type': 'SRV',
+                            'TTL': 86400,
+                            'ResourceRecords': [{
+                                'Value': '0 100 88 ipa.foo.bar.'
+                            }]
+                        }
+                    }]
+                }
+            ),
+            mock.mock.call(
+                HostedZoneId='hosted-zone-id',
+                ChangeBatch={
+                    'Changes': [{
+                        'Action': 'DELETE',
+                        'ResourceRecordSet': {
+                            'Name': '_kerberos-master._udp.foo.bar.',
+                            'Type': 'SRV',
+                            'TTL': 86400,
+                            'ResourceRecords': [{
+                                'Value': '0 100 88 ipa.foo.bar.'
+                            }]
+                        }
+                    }]
+                }
+            ),
+            mock.mock.call(
+                HostedZoneId='hosted-zone-id',
+                ChangeBatch={
+                    'Changes': [{
+                        'Action': 'DELETE',
+                        'ResourceRecordSet': {
+                            'Name': '_kerberos._tcp.foo.bar.',
+                            'Type': 'SRV',
+                            'TTL': 86400,
+                            'ResourceRecords': [{
+                                'Value': '0 100 88 ipa.foo.bar.'
+                            }]
+                        }
+                    }]
+                }
+            ),
+            mock.mock.call(
+                HostedZoneId='hosted-zone-id',
+                ChangeBatch={
+                    'Changes': [{
+                        'Action': 'DELETE',
+                        'ResourceRecordSet': {
+                            'Name': '_kerberos._udp.foo.bar.',
+                            'Type': 'SRV',
+                            'TTL': 86400,
+                            'ResourceRecords': [{
+                                'Value': '0 100 88 ipa.foo.bar.'
+                            }]
+                        }
+                    }]
+                }
+            ),
+            mock.mock.call(
+                HostedZoneId='hosted-zone-id',
+                ChangeBatch={
+                    'Changes': [{
+                        'Action': 'DELETE',
+                        'ResourceRecordSet': {
+                            'Name': '_kpasswd._tcp.foo.bar.',
+                            'Type': 'SRV',
+                            'TTL': 86400,
+                            'ResourceRecords': [{
+                                'Value': '0 100 464 ipa.foo.bar.'
+                            }]
+                        }
+                    }]
+                }
+            ),
+            mock.mock.call(
+                HostedZoneId='hosted-zone-id',
+                ChangeBatch={
+                    'Changes': [{
+                        'Action': 'DELETE',
+                        'ResourceRecordSet': {
+                            'Name': '_kpasswd._udp.foo.bar.',
+                            'Type': 'SRV',
+                            'TTL': 86400,
+                            'ResourceRecords': [{
+                                'Value': '0 100 464 ipa.foo.bar.'
+                            }]
+                        }
+                    }]
+                }
+            ),
+            mock.mock.call(
+                HostedZoneId='hosted-zone-id',
+                ChangeBatch={
+                    'Changes': [{
+                        'Action': 'DELETE',
+                        'ResourceRecordSet': {
+                            'Name': '_ldap._tcp.foo.bar.',
+                            'Type': 'SRV',
+                            'TTL': 86400,
+                            'ResourceRecords': [{
+                                'Value': '0 100 389 ipa.foo.bar.'
+                            }]
+                        }
+                    }]
+                }
+            ),
+            mock.mock.call(
+                HostedZoneId='hosted-zone-id',
+                ChangeBatch={
+                    'Changes': [{
+                        'Action': 'DELETE',
+                        'ResourceRecordSet': {
+                            'Name': '_ntp._udp.foo.bar.',
+                            'Type': 'SRV',
+                            'TTL': 86400,
+                            'ResourceRecords': [{
+                                'Value': '0 100 123 ipa.foo.bar.'
+                            }]
+                        }
+                    }]
+                }
+            ),
+            mock.mock.call(
+                HostedZoneId='hosted-zone-id',
+                ChangeBatch={
+                    'Changes': [{
+                        'Action': 'DELETE',
+                        'ResourceRecordSet': {
+                            'Name': 'ipa-ca.foo.bar.',
+                            'Type': 'A',
+                            'TTL': 86400,
+                            'ResourceRecords': [{
+                                'Value': '1.1.1.1'
+                            }]
+                        }
+                    }]
+                }
+            ),
+            mock.mock.call(
+                HostedZoneId='hosted-zone-id',
+                ChangeBatch={
+                    'Changes': [{
+                        'Action': 'DELETE',
+                        'ResourceRecordSet': {
+                            'Name': '_kerberos.foo.bar.',
+                            'Type': 'TXT',
+                            'TTL': 86400,
+                            'ResourceRecords': [{
+                                'Value': '"FOO.BAR"'
+                            }]
+                        }
+                    }]
+                }
+            )
+        ]
+        self.assertCountEqual(
+            conn_mock.change_resource_record_sets.mock_calls,
+            expected_calls
+        )
--- treadmill/infra/setup/ipa.py
+++ treadmill/infra/setup/ipa.py
@@ -8,10 +8,18 @@ class IPA:
     def __init__(self):
         pass
 
-    def setup(self, name, image_id, count, subnet_id, vpc_id, domain):
+    def setup(self,
+              name,
+              image_id,
+              count,
+              subnet_id,
+              vpc_id,
+              domain,
+              region_name):
         self.conn = connection.Connection('route53')
         self.name = name
         self.domain = domain
+        self.region_name = region_name
         self.instances = instances.Instances.create(
             name=self.name,
             image_id=image_id,
@@ -20,15 +28,29 @@ class IPA:
             instance_type=constants.INSTANCE_TYPES['EC2']['medium']
         )
 
-        self._update_route53('UPSERT', vpc_id, domain)
+        self._update_route53('UPSERT', vpc_id)
 
-    def destroy(self):
+    def destroy(self, instance_id, vpc_id, domain, region_name, name):
         """Terminate ipa instance"""
-        self.instances.terminate()
+        self.conn = connection.Connection('route53')
+        self.name = name
+        self.domain = domain
+        self.region_name = region_name
+        self.instances = instances.Instances.get(ids=[instance_id])
+        self._update_route53('DELETE', vpc_id)
+        self.instances.terminate(
+            hosted_zone_id=self.vpc.hosted_zone_id,
+            reverse_hosted_zone_id=self.vpc.reverse_hosted_zone_id,
+            domain=self.domain
+        )
 
-    def _update_route53(self, action, vpc_id, domain):
-        _vpc = vpc.VPC(vpc_id)
-        _vpc.get_hosted_zone_ids()
+    def _update_route53(self, action, vpc_id):
+        self.vpc = vpc.VPC(
+            id=vpc_id,
+            domain=self.domain,
+            region_name=self.region_name
+        )
+        self.vpc.get_hosted_zone_ids()
 
         srv_records = {
             '_kerberos-master._tcp': '0 100 88',
@@ -44,24 +66,23 @@ class IPA:
         for _rec, _value in srv_records.items():
             self._change_srv_record(
                 action=action,
-                hosted_zone_id=_vpc.hosted_zone_id,
+                hosted_zone_id=self.vpc.hosted_zone_id,
                 name=self._rec_name(_rec),
                 value=self._srv_rec_value(_value),
                 record_type='SRV'
             )
-
         self._change_srv_record(
             action=action,
-            hosted_zone_id=_vpc.hosted_zone_id,
+            hosted_zone_id=self.vpc.hosted_zone_id,
             name=self._rec_name('ipa-ca'),
             value=self.instances.instances[0].private_ip,
             record_type='A'
         )
         self._change_srv_record(
             action=action,
-            hosted_zone_id=_vpc.hosted_zone_id,
+            hosted_zone_id=self.vpc.hosted_zone_id,
             name=self._rec_name('_kerberos'),
-            value='"{0}"'.format(domain.upper()),
+            value='"{0}"'.format(self.domain.upper()),
             record_type='TXT'
         )
 
