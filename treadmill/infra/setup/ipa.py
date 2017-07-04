23970c48c376079b5af216f79c82e3408d99b858
Sny <sny.aggarwal@gmail.com>
2017-07-04 12:03:21 +0530

    ipa setup can take subnet_id

--- treadmill/infra/setup/ipa.py
+++ treadmill/infra/setup/ipa.py
@@ -22,19 +22,24 @@ class IPA:
             self,
             image_id,
             count,
-            cidr_block
+            cidr_block,
+            subnet_id=None,
     ):
         self.vpc = vpc.VPC(
             id=self.vpc_id,
             domain=self.domain,
             region_name=self.region_name
         )
-        self.vpc.create_subnet(self.region_name, cidr_block)
+
+        if not subnet_id:
+            self.vpc.create_subnet(self.region_name, cidr_block)
+            subnet_id = self.vpc.subnet_ids[0]
+
         self.instances = instances.Instances.create(
             name=self.name,
             image_id=image_id,
             count=count,
-            subnet_id=self.vpc.subnet_ids[0],
+            subnet_id=subnet_id,
             instance_type=constants.INSTANCE_TYPES['EC2']['medium']
         )
         self._update_route53('UPSERT')
