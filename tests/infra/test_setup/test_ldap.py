0db9b36979f52277b94f7f100137d86a9f00cbf2
Sny <sny.aggarwal@gmail.com>
2017-07-04 12:33:17 +0530

    ldap provisioning

---
+++ tests/infra/test_setup/test_ldap.py
@@ -0,0 +1,98 @@
+"""
+Unit test for EC2 ldap.
+"""
+
+import unittest
+import mock
+
+from treadmill.infra.setup.ldap import LDAP
+
+
+class LDAPTest(unittest.TestCase):
+    """Tests EC2 LDAP"""
+
+    @mock.patch('treadmill.infra.setup.ldap.connection.Connection')
+    @mock.patch('treadmill.infra.setup.ldap.vpc.VPC')
+    @mock.patch('treadmill.infra.setup.ldap.instances.Instances')
+    def test_setup_ldap(self, InstancesMock, VPCMock, ConnectionMock):
+        instance_mock = mock.Mock(private_ip='1.1.1.1')
+        instances_mock = mock.Mock(instances=[instance_mock])
+        InstancesMock.create = mock.Mock(return_value=instances_mock)
+        conn_mock = ConnectionMock('route53')
+        _vpc_id_mock = 'vpc-id'
+        _vpc_mock = VPCMock(id=_vpc_id_mock,
+                            region_name='region',
+                            domain='foo.bar')
+        _vpc_mock.hosted_zone_id = 'hosted-zone-id'
+        _vpc_mock.reverse_hosted_zone_id = 'reverse-hosted-zone-id'
+        _vpc_mock.subnet_ids = [123]
+        ldap = LDAP(
+            name='ldap',
+            domain='foo.bar',
+            vpc_id=_vpc_id_mock,
+            region_name='region'
+        )
+        ldap.setup(
+            image_id='foo-123',
+            count=1,
+            cidr_block='cidr-block'
+        )
+
+        self.assertEqual(ldap.instances, instances_mock)
+        InstancesMock.create.assert_called_once_with(
+            image_id='foo-123',
+            name='ldap',
+            count=1,
+            subnet_id=123,
+            instance_type='t2.medium'
+        )
+        _vpc_mock.get_hosted_zone_ids.assert_called_once()
+        _vpc_mock.create_subnet.assert_called_once_with(
+            'region', 'cidr-block'
+        )
+
+        self.assertCountEqual(
+            instance_mock.upsert_dns_record.mock_calls,
+            [
+                mock.mock.call('hosted-zone-id',
+                               'foo.bar'),
+                mock.mock.call('reverse-hosted-zone-id',
+                               'foo.bar',
+                               reverse=True)
+            ]
+        )
+ 
+    @mock.patch('treadmill.infra.setup.ldap.connection.Connection')
+    @mock.patch('treadmill.infra.setup.ldap.vpc.VPC')
+    @mock.patch('treadmill.infra.setup.ldap.instances.Instances')
+    def test_ldap_destroy(self, InstancesMock, VPCMock, ConnectionMock):
+        instance_mock = mock.Mock(private_ip='1.1.1.1')
+        instances_mock = mock.Mock(instances=[instance_mock])
+        InstancesMock.get = mock.Mock(return_value=instances_mock)
+        conn_mock = ConnectionMock('route53')
+        vpc_mock = VPCMock(
+            id='vpc-id',
+            domain='foo.bar',
+            region_name='region'
+        )
+        vpc_mock.get_hosted_zone_ids = mock.Mock()
+        vpc_mock.hosted_zone_id = 'hosted-zone-id'
+        vpc_mock.reverse_hosted_zone_id = 'reverse-hosted-zone-id'
+        ldap = LDAP(
+            vpc_id='vpc-id',
+            domain='foo.bar',
+            region_name='region',
+            name='ldap'
+        )
+        ldap.instances = InstancesMock()
+        ldap.destroy(
+            instance_id='instance-id',
+        )
+
+        InstancesMock.get.assert_called_once_with(ids=['instance-id'])
+        vpc_mock.get_hosted_zone_ids.assert_called_once()
+        ldap.instances.terminate.assert_called_once_with(
+            hosted_zone_id='hosted-zone-id',
+            reverse_hosted_zone_id='reverse-hosted-zone-id',
+            domain='foo.bar'
+        )
---
+++ treadmill/infra/setup/ldap.py
@@ -0,0 +1,72 @@
+from treadmill.infra import instances
+from treadmill.infra import connection
+from treadmill.infra import vpc
+from treadmill.infra import constants
+
+
+class LDAP:
+    def __init__(
+            self,
+            name,
+            vpc_id,
+            domain,
+            region_name
+    ):
+        self.name = name
+        self.vpc_id = vpc_id
+        self.domain = domain
+        self.region_name = region_name
+
+    def setup(
+            self,
+            image_id,
+            count,
+            cidr_block,
+            subnet_id=None,
+    ):
+        self.vpc = vpc.VPC(
+            id=self.vpc_id,
+            domain=self.domain,
+            region_name=self.region_name
+        )
+
+        self.vpc.get_hosted_zone_ids()
+
+        if not subnet_id:
+            self.vpc.create_subnet(self.region_name, cidr_block)
+            subnet_id = self.vpc.subnet_ids[0]
+
+        self.instances = instances.Instances.create(
+            name=self.name,
+            image_id=image_id,
+            count=count,
+            subnet_id=subnet_id,
+            instance_type=constants.INSTANCE_TYPES['EC2']['medium']
+        )
+
+        for i in self.instances.instances:
+            i.upsert_dns_record(
+                self.vpc.hosted_zone_id,
+                self.domain
+            )
+            i.upsert_dns_record(
+                self.vpc.reverse_hosted_zone_id,
+                self.domain,
+                reverse=True
+            )
+
+    def destroy(self, instance_id):
+        """Terminate ldap instance"""
+        self.instances = instances.Instances.get(ids=[instance_id])
+        self.vpc = vpc.VPC(
+            id=self.vpc_id,
+            domain=self.domain,
+            region_name=self.region_name
+        )
+        self.vpc.get_hosted_zone_ids()
+        self.instances.terminate(
+            hosted_zone_id=self.vpc.hosted_zone_id,
+            reverse_hosted_zone_id=self.vpc.reverse_hosted_zone_id,
+            domain=self.domain
+        )
+
