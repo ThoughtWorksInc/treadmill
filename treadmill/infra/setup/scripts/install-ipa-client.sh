d4092d3fef2f56f1543985d2d49778912cb8e7eb
Villies Thekkekara <villiest@thoughtworks.com>
2017-07-05 12:50:27 +0530

    IPA client install & config userdata script

--- treadmill/infra/setup/scripts/install-ipa-client.sh
+++ treadmill/infra/setup/scripts/install-ipa-client.sh
@@ -1,8 +1,8 @@
-#install
+# install
 yum install -y ipa-client
 
-#configure
-CLIENT_HOSTNAME=$(hostname)
-CLIENT_IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+# configure
+ipa-client-install --unattended --no-ntp \
+    --mkhomedir --no-krb5-offline-password \
+    --password "{{ ONE_TIME_PASSWORD }}"
 
-grep -F -q '$CLIENT_IP $CLIENT_HOSTNAME' /etc/hosts || echo $CLIENT_IP $CLIENT_HOSTNAME >> /etc/hosts
