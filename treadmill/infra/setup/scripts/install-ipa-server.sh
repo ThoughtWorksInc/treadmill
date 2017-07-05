3849d712d083f1bce843fe4dc2c2e6a39aa712b1
Villies Thekkekara <villiest@thoughtworks.com>
2017-07-05 12:58:57 +0530

    Adding more parameters for IPA server install command

--- treadmill/infra/setup/scripts/install-ipa-server.sh
+++ treadmill/infra/setup/scripts/install-ipa-server.sh
@@ -1,6 +1,8 @@
 yum install -y ipa-server
 
-/sbin/ipa-server-install -U --domain "{{ DOMAIN }}" \
+ipa-server-install --unattended \
+    --no-ntp --mkhomedir \
+    --domain "{{ DOMAIN }}" \
     --realm "{{ DOMAIN|upper }}" \
     -a "{{ IPA_ADMIN_PASSWORD }}" \
     -p "{{ IPA_ADMIN_PASSWORD }}"
