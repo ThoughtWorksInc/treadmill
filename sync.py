d79f3004c9d7a67ae482566c10098e7cecb41f39
Sny <sny.aggarwal@gmail.com>
2017-07-21 12:36:04 +0530

    sync script fails for uncommited changes | adding logs

--- sync.py
+++ sync.py
@@ -1,15 +1,30 @@
-import subprocess
 import sys
+import subprocess
 
-branch = sys.argv[1]
-merge_branch = branch.strip() + '-merge'
-subprocess.call(['git', 'checkout', branch])
-subprocess.call(['git', 'branch', '-D', merge_branch])
-subprocess.call(['git', 'pull', '--rebase'])
-subprocess.call(['git', 'checkout', '-b', merge_branch])
-terminal_commit = sys.argv[2]
-subprocess.call(['git', 'reset', '--soft', terminal_commit])
-subprocess.call(['git', 'commit', '--amend', '--no-edit'])
-if (len(sys.argv) > 3) and sys.argv[3]:
-    subprocess.call(['git', 'commit', '--amend', '-m', sys.argv[3]])
-subprocess.call(['git', 'push', 'origin', merge_branch, '-f'])
+uncommit_changes = subprocess.check_output(
+    ['git', 'status', '--porcelain', '--untracked-files=no']
+)
+
+if uncommit_changes:
+    print(
+        'Please commit changes of the current branch, and then try again.'
+    )
+else:
+    print('Preparing...')
+    branch = sys.argv[1]
+    merge_branch = branch.strip() + '-merge'
+    subprocess.call(['git', 'checkout', branch])
+    print('Deleting old branch ' + merge_branch + ' locally...')
+    subprocess.call(['git', 'branch', '-D', merge_branch])
+    print('Pulling latest changes...')
+    subprocess.call(['git', 'pull', '--rebase'])
+    subprocess.call(['git', 'checkout', '-b', merge_branch])
+    terminal_commit = sys.argv[2]
+    print('Rebasing and squashing...')
+    subprocess.call(['git', 'reset', '--soft', terminal_commit])
+    subprocess.call(['git', 'commit', '--amend', '--no-edit'])
+    if (len(sys.argv) > 3) and sys.argv[3]:
+        subprocess.call(['git', 'commit', '--amend', '-m', sys.argv[3]])
+    print('Promoting to remote...')
+    subprocess.call(['git', 'push', 'origin', merge_branch, '-f'])
+    print('Synced!')
