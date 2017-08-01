e9d2732d637c563768107184386b6bc49f513ccb
Sny <sny.aggarwal@gmail.com>
2017-08-01 17:50:57 +0530

    added usage with help arg

--- sync.py
+++ sync.py
@@ -1,38 +1,42 @@
 import sys
 import subprocess
 
-uncommit_changes = subprocess.check_output(
-    ['git', 'status', '--porcelain', '--untracked-files=no']
-)
 
-if uncommit_changes:
-    print(
-        'Please commit changes of the current branch, and then try again.'
-    )
+if sys.argv[1] == 'help':
+    print("Usage:\npython sync.py <branch-to-be-squashed> <commit-sha-till-you-want-to-sqaus> '<commit-message>'")
 else:
-    print('Preparing...')
-    branch = sys.argv[1]
-    merge_branch = branch.strip() + '-merge'
-    subprocess.call(['git', 'checkout', branch])
-    print('Deleting old branch ' + merge_branch + ' locally...')
-    subprocess.call(['git', 'branch', '-D', merge_branch])
-    print('Pulling latest changes...')
-    subprocess.call(['git', 'pull', '--rebase'])
-    subprocess.call(['git', 'checkout', '-b', merge_branch])
-    terminal_commit = sys.argv[2]
-    print('Rebasing and squashing...')
-    subprocess.call(['git', 'reset', '--soft', terminal_commit])
-    subprocess.call(['git', 'commit', '--amend', '--no-edit'])
-    if (len(sys.argv) > 3) and sys.argv[3]:
-        subprocess.call(['git', 'commit', '--amend', '-m', sys.argv[3]])
-    print('Promoting to remote...')
+    uncommit_changes = subprocess.check_output(
+        ['git', 'status', '--porcelain', '--untracked-files=no']
+    )
 
-    yes = set(['yes','y', 'Y'])
-    choice = raw_input('Push this [Y/n]? ').lower()
-    if choice in yes:
-        subprocess.call(['git', 'push', 'origin', merge_branch, '-f'])
-        print('Synced!')
+    if uncommit_changes:
+        print(
+            'Please commit changes of the current branch, and then try again.'
+        )
     else:
-        print('Branch is rebased/squashed and ready to be pushed.')
+        print('Preparing...')
+        branch = sys.argv[1]
+        merge_branch = branch.strip() + '-merge'
+        subprocess.call(['git', 'checkout', branch])
+        print('Deleting old branch ' + merge_branch + ' locally...')
+        subprocess.call(['git', 'branch', '-D', merge_branch])
+        print('Pulling latest changes...')
+        subprocess.call(['git', 'pull', '--rebase'])
+        subprocess.call(['git', 'checkout', '-b', merge_branch])
+        terminal_commit = sys.argv[2]
+        print('Rebasing and squashing...')
+        subprocess.call(['git', 'reset', '--soft', terminal_commit])
+        subprocess.call(['git', 'commit', '--amend', '--no-edit'])
+        if (len(sys.argv) > 3) and sys.argv[3]:
+            subprocess.call(['git', 'commit', '--amend', '-m', sys.argv[3]])
+        print('Promoting to remote...')
 
-    print('\x1b[6;30;42m' + 'Good Job!!!' + '\x1b[0m')
+        yes = set(['yes','y', 'Y'])
+        choice = raw_input('Push this [Y/n]? ').lower()
+        if choice in yes:
+            subprocess.call(['git', 'push', 'origin', merge_branch, '-f'])
+            print('Synced!')
+        else:
+            print('Branch is rebased/squashed and ready to be pushed.')
+
+        print('\x1b[6;30;42m' + 'Good Job!!!' + '\x1b[0m')
