b0a3e9650e7ec280a59f6f8c6f5d8c3d51e152ee
krajat <krajat@thoughtworks.com>
2017-08-01 17:02:30 +0530

    Revert "+pooja-dhupar | Add ACL methods in zk plugin"
    This reverts commit 6472917ea5f28dcee7c3cefabd30aad6830ab8fe.

--- tests/plugins/zookeeper_test.py
+++ tests/plugins/zookeeper_test.py
@@ -1,9 +1,10 @@
 """
 Unit test for zookeeper plugin
 """
+
 import unittest
 import mock
-from treadmill.plugins import zookeeper
+from treadmill.plugins.zookeeper import connect
 
 
 class ZookeeperTest(unittest.TestCase):
@@ -11,7 +12,7 @@ class ZookeeperTest(unittest.TestCase):
     def test_connect_without_connargs(self, kazooClientMock):
         zkurl = 'zookeeper://foo@123:21'
 
-        zookeeper.connect(zkurl, {})
+        connect(zkurl, {})
 
         kazooClientMock.assert_called_once_with(
             hosts='foo@123:21',
@@ -31,7 +32,7 @@ class ZookeeperTest(unittest.TestCase):
             }
         }
 
-        zookeeper.connect(zkurl, connargs)
+        connect(zkurl, connargs)
 
         kazooClientMock.assert_called_once_with(
             hosts='foobar:123',
@@ -39,34 +40,3 @@ class ZookeeperTest(unittest.TestCase):
                 'service': 'foo',
                 'mechanisms': 'bar'
             })
-
-    @mock.patch('kazoo.security.make_acl')
-    def test_make_user_acl(self, makeAclMock):
-        zookeeper.make_user_acl('foo', 'rw')
-
-        makeAclMock.assert_called_once_with(
-            scheme='sasl', credential='foo', read=True,
-            write=True, create=False, delete=False, admin=False
-        )
-
-    @mock.patch('kazoo.security.make_acl')
-    def test_make_role_acl_with_valid_role(self, makeAclMock):
-        zookeeper.make_role_acl('servers', 'ra')
-
-        makeAclMock.assert_called_once_with(
-            scheme='sasl', credential='role/servers', read=True,
-            write=False, delete=False, create=False, admin=True
-        )
-
-    def test_make_role_acl_with_invalid_role(self):
-        with self.assertRaises(AssertionError):
-            zookeeper.make_role_acl('foo', 'rwc')
-
-    @mock.patch('kazoo.security.make_acl')
-    def test_make_host_acl(self, makeAclMock):
-        zookeeper.make_host_acl('foo@123', 'rdwca')
-
-        makeAclMock.assert_called_once_with(
-            scheme='sasl', credential='host/foo@123', read=True,
-            write=True, delete=True, create=True, admin=True
-        )
