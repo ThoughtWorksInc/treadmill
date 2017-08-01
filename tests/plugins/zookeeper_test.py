631d92355c8ec6d25f4c40bf087a278ad9c31ce2
krajat <krajat@thoughtworks.com>
2017-08-01 17:02:27 +0530

    Revert "+@pooja-dhupar | Zookeeper test fix"
    This reverts commit f841be2e1ee0cb7886b7f6767380dd65d914dc2c.

--- tests/plugins/zookeeper_test.py
+++ tests/plugins/zookeeper_test.py
@@ -14,7 +14,7 @@ class ZookeeperTest(unittest.TestCase):
         zookeeper.connect(zkurl, {})
 
         kazooClientMock.assert_called_once_with(
-            hosts='123:21',
+            hosts='foo@123:21',
             sasl_data={
                 'service': 'host',
                 'mechanisms': ['GSSAPI']
