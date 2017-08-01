2f95151e394b068fc3e90b17a3dc38fb7e21c27a
Sny <sny.aggarwal@gmail.com>
2017-08-01 17:59:45 +0530

    added usage with help arg in helper scripts

--- treadmill/infra/helper_scripts/disable_ssh.py
+++ treadmill/infra/helper_scripts/disable_ssh.py
@@ -2,14 +2,18 @@ from urllib import request
 from treadmill.infra import connection
 import sys
 
-my_ip = request.urlopen(
-    'http://ip.42.pl/raw'
-).read().decode('utf-8') + '/32'
-conn = connection.Connection()
-conn.revoke_security_group_ingress(
-    CidrIp=my_ip,
-    FromPort=22,
-    ToPort=22,
-    GroupId=sys.argv[1],
-    IpProtocol='tcp'
-)
+
+if sys.argv[1] == 'help':
+    print("Usage:\n python disable_ssh.py <secgroup-id>")
+else:
+    my_ip = request.urlopen(
+        'http://ip.42.pl/raw'
+    ).read().decode('utf-8') + '/32'
+    conn = connection.Connection()
+    conn.revoke_security_group_ingress(
+        CidrIp=my_ip,
+        FromPort=22,
+        ToPort=22,
+        GroupId=sys.argv[1],
+        IpProtocol='tcp'
+    )
