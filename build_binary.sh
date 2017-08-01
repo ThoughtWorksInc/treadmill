fc07d75c58b67a4e1f603873d352a727fddfc6db
Sny <sny.aggarwal@gmail.com>
2017-08-01 17:45:13 +0530

    checking out kazoo in current dir

--- build_binary.sh
+++ build_binary.sh
@@ -9,12 +9,14 @@ pip install git+https://github.com/thoughtworksinc/pex#egg=pex
 
 mkdir -p dist
 mkdir -p ~/rpmbuild/SOURCES/treadmill-0.1
-# This needs to go away after ceache kazoo patch is merged
-if [ ! -d "/tmp/kazoo" ]; then
-    git clone -b puresasl https://github.com/ceache/kazoo.git /tmp/kazoo
+
+#This needs to go away after ceache kazoo patch is merged and released
+if [ ! -d "kazoo" ]; then
+    git clone -b puresasl https://github.com/ceache/kazoo.git
 fi
 
-pex . /tmp/kazoo -o dist/treadmill.pex -e treadmill.console:run -v
+pex . kazoo -o dist/treadmill.pex -e treadmill.console:run -v
+rm -rf kazoo
 
 cp dist/treadmill.pex ~/rpmbuild/SOURCES/treadmill-0.1/treadmill
 cp etc/treadmill.spec ~/rpmbuild/SPECS/
