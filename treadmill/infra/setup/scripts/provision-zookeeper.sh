3fd33ca5d530091c40dde82506fed47fa0e9e134
krajat <krajat@thoughtworks.com>
2017-08-04 12:39:27 +0530

    + @ankitabhopatkar13 | Fixed hostname issue. Create myid file after zk initialize

--- treadmill/infra/setup/scripts/provision-zookeeper.sh
+++ treadmill/infra/setup/scripts/provision-zookeeper.sh
@@ -9,11 +9,6 @@ yum -y install zookeeper-ldap-plugin --nogpgcheck
 
 # Configure
 
-AMI_LAUNCH_INDEX=$(curl http://169.254.169.254/latest/meta-data/ami-launch-index)
-ZK_ID=$((AMI_LAUNCH_INDEX+1))
-
-echo $ZK_ID > /var/lib/zookeeper/myid
-
 (
 cat <<EOF
 server.1=TreadmillZookeeper1.{{ DOMAIN }}:2888:3888
@@ -24,13 +19,14 @@ EOF
 
 mac_addr=`cat /sys/class/net/eth0/address`
 subnet_id=`curl http://169.254.169.254/latest/meta-data/network/interfaces/macs/$mac_addr/subnet-id`
+HOST_FQDN=$(hostname -f)
 
 export TREADMILL_CELL=$subnet_id
 
 envsubst < /etc/zookeeper/conf/treadmill.conf > /etc/zookeeper/conf/temp.conf
 mv /etc/zookeeper/conf/temp.conf /etc/zookeeper/conf/treadmill.conf -f
 sed -i s/REALM/{{ DOMAIN|upper }}/g /etc/zookeeper/conf/treadmill.conf
-sed -i s/PRINCIPAL/'"'host\\/$HOSTNAME'"'/g /etc/zookeeper/conf/jaas.conf
+sed -i s/PRINCIPAL/'"'host\\/$HOST_FQDN'"'/g /etc/zookeeper/conf/jaas.conf
 
 (
 cat <<EOF
@@ -53,5 +49,13 @@ EOF
 ) > /etc/systemd/system/zookeeper.service
 
 chown -R zookeeper:zookeeper /var/lib/zookeeper
+
+zookeeper-server-initialize
+
+AMI_LAUNCH_INDEX=$(curl http://169.254.169.254/latest/meta-data/ami-launch-index)
+ZK_ID=$((AMI_LAUNCH_INDEX+1))
+
+echo $ZK_ID > /var/lib/zookeeper/myid
+
 /bin/systemctl enable zookeeper.service
 /bin/systemctl start zookeeper.service
