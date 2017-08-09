09c4f873551cfd17090b85d950ae75a2c107c465
Sny <sny.aggarwal@gmail.com>
2017-08-09 13:08:03 +0530

    env_vars are created in /etc as well for container mount

--- vagrant/scripts/env_vars.sh
+++ vagrant/scripts/env_vars.sh
@@ -1,5 +1,8 @@
 # Configure environment variables.
 
+# Setup environment vars
+(
+    cat <<EOF
 export TREADMILL_ZOOKEEPER=zookeeper://foo@master:2181
 export TREADMILL_CELL=local
 export TREADMILL_LDAP=ldap://master:22389
@@ -9,5 +12,8 @@ export TREADMILL_LDAP_USER=cn=Manager,cn=config
 export TREADMILL=/opt/treadmill
 export LC_ALL=en_US.utf8
 export LANG=en_US.utf8
-
 export PATH=$PATH:/opt/s6/bin:/opt/treadmill/bin
+EOF
+) >> /etc/profile.d/treadmill_profile.sh
+
+source /etc/profile.d/treadmill_profile.sh
