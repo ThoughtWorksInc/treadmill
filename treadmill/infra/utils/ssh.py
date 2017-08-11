a2de46f3fd53cb1550c581c72350e5eb098ff2d9
krajat <krajat@thoughtworks.com>
2017-08-11 16:48:10 +0530

    + @ankitabhopatkar13 | Moved conn object to methods

--- treadmill/infra/utils/ssh.py
+++ treadmill/infra/utils/ssh.py
@@ -1,13 +1,13 @@
 from urllib import request
 from treadmill.infra import connection
 
-conn = connection.Connection()
 my_ip = request.urlopen(
     'http://ip.42.pl/raw'
 ).read().decode('utf-8') + '/32'
 
 
 def enable(group_id):
+    conn = connection.Connection()
     conn.authorize_security_group_ingress(
         CidrIp=my_ip,
         FromPort=22,
@@ -18,6 +18,7 @@ def enable(group_id):
 
 
 def disable(group_id):
+    conn = connection.Connection()
     conn.revoke_security_group_ingress(
         CidrIp=my_ip,
         FromPort=22,
