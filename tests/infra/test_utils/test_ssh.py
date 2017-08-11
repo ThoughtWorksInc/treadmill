5c828911086a15b41285c2384dfa66be55c53566
krajat <krajat@thoughtworks.com>
2017-08-11 16:48:10 +0530

    + @ankitabhopatkar13 | Moved conn object to methods

--- tests/infra/test_utils/test_ssh.py
+++ tests/infra/test_utils/test_ssh.py
@@ -6,12 +6,12 @@ from treadmill.infra.utils import ssh
 class SSHTest(unittest.TestCase):
     @mock.patch('treadmill.infra.connection.Connection')
     def test_enable(self, connectionMock):
-        ssh.conn = connectionMock()
+        conn = connectionMock()
         ssh.my_ip = '127.0.0.1'
 
         ssh.enable(group_id='sg-123')
 
-        ssh.conn.authorize_security_group_ingress.assert_called_once_with(
+        conn.authorize_security_group_ingress.assert_called_once_with(
             CidrIp='127.0.0.1',
             FromPort=22,
             ToPort=22,
@@ -21,12 +21,12 @@ class SSHTest(unittest.TestCase):
 
     @mock.patch('treadmill.infra.connection.Connection')
     def test_disable(self, connectionMock):
-        ssh.conn = connectionMock()
+        conn = connectionMock()
         ssh.my_ip = '127.0.0.1'
 
         ssh.disable(group_id='sg-123')
 
-        ssh.conn.revoke_security_group_ingress.assert_called_once_with(
+        conn.revoke_security_group_ingress.assert_called_once_with(
             CidrIp='127.0.0.1',
             FromPort=22,
             ToPort=22,
