92a4621c2e4ccd96ed0d4feabc3728e611e887e5
Villies Thekkekara <villiest@thoughtworks.com>
2017-09-20 13:35:40 +0530

    IPA server setup for vagrant

--- vagrant/scripts/configure-etc-hosts.sh
+++ vagrant/scripts/configure-etc-hosts.sh
@@ -3,3 +3,4 @@
 echo 127.0.0.1   localhost   > /etc/hosts
 echo 10.10.10.10 master     >> /etc/hosts
 echo 10.10.10.11 node       >> /etc/hosts
+echo 10.10.10.12 ipa.ms.local    ipa    >> /etc/hosts
