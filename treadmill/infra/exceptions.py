5bf78a3521c6d508f256e20fbe98ff04e1ff33b7
Sny <sny.aggarwal@gmail.com>
2017-10-06 13:23:26 +0530

    added exception messages

--- treadmill/infra/exceptions.py
+++ treadmill/infra/exceptions.py
@@ -2,8 +2,7 @@ class LDAPNotFound(Exception):
     """When LDAP server is not provisioned."""
 
     def __init__(self):
-        self.message = 'LDAPNotFound: \
-        Please check if LDAP Server is up and running.'
+        self.message = 'LDAPNotFound: Please check if LDAP Server is up and running.' # noqa
 
     def __str__(self):
         return self.message
@@ -13,8 +12,7 @@ class IPAServerNotFound(Exception):
     """When IPA server is not provisioned"""
 
     def __init__(self):
-        self.message = 'IPAServerNotFound: \
-        Please check if IPA Server is up and running.'
+        self.message = 'IPAServerNotFound: Please check if IPA Server is up and running.' # noqa
 
     def __str__(self):
         return self.message
